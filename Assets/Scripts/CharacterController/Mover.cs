f1e4cbc329756c4522b91b205490ac457974b79a
Iride Lombardi <lorenzomassidda@hotmail.com>
2022-03-20 12:50:56 +0100

    Mover progress

--- Assets/Scripts/CharacterController/Mover.cs
+++ Assets/Scripts/CharacterController/Mover.cs
@@ -50,42 +50,195 @@ namespace Tirocinio
             stepHeightRatio = _newStepHeightRatio;
         }
 
-        const float groundDistance = 0.5f;
         const float raySensorDistance = 0.5f;
 
         const float sphereCastRadius = 0.4f;
         bool _isGrounded = false;
 
-        Vector3? hitPoint = null;
+
+        public struct Raycast
+        {
+            public Raycast(Vector3 origin, Vector3 direction, float distance)
+            {
+                this.origin = origin;
+                this.direction = direction;
+                this.distance = distance;
+            }
+            public Vector3 origin;
+            public Vector3 direction;
+            public float distance;
+
+            public bool Cast(LayerMask mask)
+            {
+                if (Physics.Raycast(origin, direction, out RaycastHit hit, distance, ~mask))
+                {
+                    distance = hit.distance;
+                    return true;
+                }
+                return false;
+            }
+
+            public bool Cast(LayerMask mask, out RaycastHit hit)
+            {
+                if (Physics.Raycast(origin, direction, out hit, distance, ~mask))
+                {
+                    distance = hit.distance;
+                    return true;
+                }
+                return false;
+            }
+
+            public bool SphereCast(float radius, LayerMask mask)
+            {
+                if (Physics.SphereCast(origin, radius, direction, out RaycastHit hit, distance, ~mask))
+                {
+                    distance = hit.distance;
+                    return true;
+                }
+                return false;
+            }
+
+            public bool SphereCast(float radius, LayerMask mask, out RaycastHit hit)
+            {
+                if (Physics.SphereCast(origin, radius, direction, out hit, distance, ~mask))
+                {
+                    distance = hit.distance;
+                    return true;
+                }
+                return false;
+            }
+
+
+        }
+        Raycast currentRaycast;
+
+        Vector3 groundPoint;
+        Vector3 groundNormal;
 
         public void CheckForGround()
         {
-            Vector3 bottom = transform.position + colliderOffset;
-            bottom.y -= colliderHeight * 0.5f;
+
+            Vector3 origin = transform.position + colliderOffset;
             LayerMask mask = gameObject.layer;
             RaycastHit hit;
             switch (sensorType)
             {
                 case SensorType.Raycast:
-                    if (Physics.Raycast(bottom, -transform.up, out hit, groundDistance, ~mask))
+                    currentRaycast = new Raycast(origin, -transform.up, colliderHeight * 0.6f);
+
+                    _isGrounded = currentRaycast.Cast(mask, out hit);
+                    if (_isGrounded)
                     {
-                        _isGrounded = true;
-                        hitPoint = hit.point;
+                        groundPoint = hit.point;
+                        groundNormal = hit.normal;
                     }
-                    else hitPoint = null;
+
                     break;
                 case SensorType.Spherecast:
-                    if (Physics.SphereCast(bottom, sphereCastRadius, -transform.up, out hit, groundDistance, ~mask))
+                    currentRaycast = new Raycast(origin, -transform.up, colliderHeight * 0.6f);
+
+                    _isGrounded = currentRaycast.SphereCast(sphereCastRadius, mask, out hit);
+                    if (_isGrounded)
                     {
-                        _isGrounded = true;
-                        hitPoint = hit.point;
+                        groundPoint = hit.point;
+                        groundNormal = hit.normal;
                     }
-                    else hitPoint = null;
+
+                    break;
+                case SensorType.RaycastArray:
+                    _isGrounded = RaycastArray(origin, colliderThickness,ref groundPoint, ref groundNormal);
 
                     break;
             }
 
         }
+
+
+
+        List<Raycast> raycastArray = new List<Raycast>();
+
+        bool RaycastArray(Vector3 center, float radius, ref Vector3 position, ref Vector3 normal)
+        {
+            bool grounded = false;
+            List<Vector3> points = new List<Vector3>();
+            List<Vector3> normals = new List<Vector3>();
+            raycastArray = new List<Raycast>();
+
+            LayerMask mask = gameObject.layer;
+
+
+            Raycast ray = new Raycast(center, -transform.up, colliderHeight * 0.6f);
+            //center ray
+            if (ray.Cast(mask, out RaycastHit hit))
+            {
+                points.Add(hit.point);
+                normals.Add(hit.normal);
+                grounded = true;
+            }
+            raycastArray.Add(ray);
+
+
+            for (int i = 1; i <= sensorArrayRows; i++)
+            {
+                float distance = ((float)i / sensorArrayRows) * radius;
+
+                for (int j = 0; j < sensorArrayRayCount; j++)
+                {
+                    float deltaAngle = 360f / sensorArrayRayCount;
+                    float angle = j * deltaAngle;
+
+                    if (sensorArrayRowsAreOffset)
+                    {
+                        bool isRowOdd = i % 2 == 1;
+                        if (!isRowOdd)
+                        {
+                            angle += deltaAngle * 0.5f;
+                        }
+                    }
+
+                    angle *= Mathf.Deg2Rad;
+
+                    Vector3 circlePos = new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle));
+                    Vector3 worldPos = center + circlePos * distance;
+
+                    ray = new Raycast(worldPos, -transform.up, colliderHeight * 0.6f);
+
+                    if (ray.Cast(mask, out RaycastHit hit2))
+                    {
+                        points.Add(hit2.point);
+                        normals.Add(hit2.normal);
+                        grounded = true;
+                    }
+                    raycastArray.Add(ray);
+
+
+                }
+            }
+            if (grounded)
+            {
+                position = Average(points);
+                normal = Average(normals).normalized;
+            }
+
+            return grounded;
+
+        }
+
+        Vector3 Average(List<Vector3> vectors)
+        {
+            Vector3 average = Vector3.zero;
+
+            foreach (Vector3 v in vectors)
+            {
+                average += v;
+            }
+
+            average /= vectors.Count;
+
+            return average;
+        }
+
+
 
         void DrawGreenCross(Vector3 point, float size)
         {
@@ -107,7 +260,7 @@ namespace Tirocinio
 
         public bool IsGrounded()
         {
-            return false;
+            return _isGrounded;
         }
 
         public void SetExtendSensorRange(bool value)
@@ -124,46 +277,63 @@ namespace Tirocinio
 
         private void OnDrawGizmos()
         {
-            Vector3 bottom = transform.position + colliderOffset;
-            bottom.y -= colliderHeight * 0.5f;
+            Vector3 origin = transform.position + colliderOffset;
             if (isInDebugMode)
             {
                 switch (sensorType)
                 {
                     case SensorType.Raycast:
                         Gizmos.color = Color.red;
-                        if (!_isGrounded || hitPoint == null)
+                        Gizmos.DrawRay(currentRaycast.origin, currentRaycast.direction * currentRaycast.distance);
+                        if (_isGrounded)
                         {
-                            Gizmos.DrawRay(bottom,-transform.up * groundDistance);
-                        }
-                        else
-                        {
-                            Gizmos.DrawLine(bottom, (Vector3)hitPoint);
                             Gizmos.color = Color.green;
-                            DrawGreenCross((Vector3)hitPoint, 0.2f);
+                            DrawGreenCross(groundPoint, 0.2f);
+                            Gizmos.color = Color.blue;
+                            Gizmos.DrawRay(groundPoint, groundNormal);
                         }
+
+
                         break;
                     case SensorType.Spherecast:
                         Gizmos.color = Color.red;
-                        Gizmos.DrawWireSphere(bottom, sphereCastRadius);
-                        if (!_isGrounded || hitPoint == null)
+                        Gizmos.DrawWireSphere(currentRaycast.origin, sphereCastRadius);
+                        Gizmos.DrawRay(currentRaycast.origin, currentRaycast.direction * currentRaycast.distance);
+
+                        if (_isGrounded)
                         {
-                            Gizmos.DrawRay(bottom, transform.up * groundDistance);
-                            Gizmos.DrawWireSphere(bottom - transform.up * groundDistance, sphereCastRadius);
-                        }
-                        else
-                        {
-                            Gizmos.DrawLine(bottom, (Vector3)hitPoint);
+                            Gizmos.color = Color.blue;
+                            Gizmos.DrawRay(groundPoint, groundNormal);
+
                             Gizmos.color = Color.green;
-                            Gizmos.DrawWireSphere((Vector3)hitPoint, sphereCastRadius);
+                        }
+
+                        Gizmos.DrawWireSphere(currentRaycast.origin + currentRaycast.direction * currentRaycast.distance, sphereCastRadius);
+
+
+                        break;
+                    case SensorType.RaycastArray:
+                        Gizmos.color = Color.red;
+                        foreach (Raycast raycast in raycastArray)
+                        {
+                            Gizmos.DrawRay(raycast.origin, raycast.direction * raycast.distance);
+                        }
+
+                        if (_isGrounded)
+                        {
+                            DrawGreenCross(groundPoint, 0.2f);
+
+                            Gizmos.color = Color.blue;
+                            Gizmos.DrawRay(groundPoint, groundNormal);
                         }
                         break;
                 }
             }
         }
-
-
-
-
     }
+
+
+
+
 }
+
